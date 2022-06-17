62f1724fdff3ec14aca9c46cd684f53492a40286
NotArtwork <105832230+NotArtwork@users.noreply.github.com>
2022-06-16 21:27:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name : "Sam",
+    address: "37 Planet Cool",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = {...employee};
+    delete updatedEmployee.name;
+    return updatedEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee.name
+   return employee
+}
